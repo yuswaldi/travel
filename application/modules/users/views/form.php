c41cdbe02b120f8efdcd435f57e8cbe989fc5261
yudhi <yuswaldi@gmail.com>
2014-08-14 15:21:02 +0700

    form changes

--- application/modules/users/views/form.php
+++ application/modules/users/views/form.php
@@ -12,7 +12,9 @@
                 <div class="col-md-12">
                     <div class="box box-primary">
                         <div class="box-body">
-                            
+                            <div>
+                                
+                            </div>
                         </div>
                     </div>
                 </div>
